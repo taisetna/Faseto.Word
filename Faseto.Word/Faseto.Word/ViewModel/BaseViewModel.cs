37697130a095eb63147741c4ddd43aec4870946b
ChangHyunChoi <taisetna@nate.com>
2019-06-10 22:18:52 +0900

    추가: Styles(Xml), Fonts, Images, ViewModel 등등

---
+++ Faseto.Word/Faseto.Word/ViewModel/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using PropertyChanged;
+using System.ComponentModel;
+
+namespace WpfTreeView
+{
+    [ImplementPropertyChanged]
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => {};
+        
+        public void OnPropertyChanged(string _name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(_name));
+        }
+    }
+}
---
+++ Faseto.Word/Faseto.Word/ViewModel/RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Faseto.Word
+{
+    class RelayCommand : ICommand
+    {
+        private Action m_Action { set; get; }
+
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+
+        public RelayCommand(Action _action)
+        {
+            m_Action = _action;
+        }
+
+        public bool CanExecute(object _parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object _parameter)
+        {
+            m_Action();
+        }
+    }
+}
