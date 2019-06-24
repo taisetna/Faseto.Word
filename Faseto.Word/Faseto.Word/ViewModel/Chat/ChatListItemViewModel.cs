9189f5fb37a2039d8dcab81d2e0cfac481504d3a
ChangHyun Choi <hessorikng@hanmail.net>
2019-06-24 21:11:41 +0900

    ChatListControl, ChatListItemViewModel 추가

---
+++ Faseto.Word/Faseto.Word/ViewModel/Chat/ChatListItemViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Fasetto.Word
+{
+    public class ChatListItemViewModel : BaseViewModel
+    {
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public string Initials { get; set; }
+        public string ProfilePictureRGB { get; set; }
+    }
+}
---
+++ Faseto.Word/Faseto.Word/ViewModel/Chat/Design/ChatListItemDesignModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Fasetto.Word
+{
+    public class ChatListItemDesignModel : ChatListItemViewModel
+    {
+        #region Singleton
+        public static ChatListItemDesignModel Instance => new ChatListItemDesignModel();
+        #endregion
+
+        #region Constructor
+        public ChatListItemDesignModel()
+        {
+            Initials = "LM";
+            Name = "Luke";
+            Message = "This chat app is awesome! I bet it will be fast too";
+            ProfilePictureRGB = "3099c5";
+         }
+        #endregion
+    }
+}
