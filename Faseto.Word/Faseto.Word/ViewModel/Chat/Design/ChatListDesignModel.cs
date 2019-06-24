6dc5e758c21102c8445a47dd56dd339926124137
ChangHyun Choi <hessorikng@hanmail.net>
2019-06-24 21:39:28 +0900

    ContentStyle 추가

--- Faseto.Word/Faseto.Word/ViewModel/Chat/Design/ChatListDesignModel.cs
+++ Faseto.Word/Faseto.Word/ViewModel/Chat/Design/ChatListDesignModel.cs
@@ -34,6 +34,48 @@ namespace Fasetto.Word
                     Message = "The new server is up, got 192.168.1.1",
                     ProfilePictureRGB = "00d405"
                 },
+                  new ChatListItemViewModel
+                {
+                    Name = "Luke",
+                    Initials = "LM",
+                    Message = "This chat app is awesome! I bet it will be fast too.",
+                    ProfilePictureRGB = "3099c5"
+                },
+                new ChatListItemViewModel
+                {
+                    Name = "Jesse",
+                    Initials = "JA",
+                    Message = "Hey dude, here are the new icons",
+                    ProfilePictureRGB = "fe4503"
+                },
+                new ChatListItemViewModel
+                {
+                    Name = "Parnell",
+                    Initials = "PL",
+                    Message = "The new server is up, got 192.168.1.1",
+                    ProfilePictureRGB = "00d405"
+                },
+                  new ChatListItemViewModel
+                {
+                    Name = "Luke",
+                    Initials = "LM",
+                    Message = "This chat app is awesome! I bet it will be fast too.",
+                    ProfilePictureRGB = "3099c5"
+                },
+                new ChatListItemViewModel
+                {
+                    Name = "Jesse",
+                    Initials = "JA",
+                    Message = "Hey dude, here are the new icons",
+                    ProfilePictureRGB = "fe4503"
+                },
+                new ChatListItemViewModel
+                {
+                    Name = "Parnell",
+                    Initials = "PL",
+                    Message = "The new server is up, got 192.168.1.1",
+                    ProfilePictureRGB = "00d405"
+                },
             };
         }
         #endregion
