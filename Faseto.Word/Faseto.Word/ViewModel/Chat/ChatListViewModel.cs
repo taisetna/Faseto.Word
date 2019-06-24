8555dc5f29d193fe5b66ad74a0d73a5fc89894aa
ChangHyun Choi <hessorikng@hanmail.net>
2019-06-24 21:33:12 +0900

    ChatListViewModel, ChatListDesignModel, StringRGBtoBrushConverter 추가

---
+++ Faseto.Word/Faseto.Word/ViewModel/Chat/ChatListViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Fasetto.Word
+{
+    public class ChatListViewModel : BaseViewModel
+    {
+        public List<ChatListItemViewModel> Items { get; set; }
+    }
+}
---
+++ Faseto.Word/Faseto.Word/ViewModel/Chat/Design/ChatListDesignModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace Fasetto.Word
+{
+    public class ChatListDesignModel : ChatListViewModel
+    {
+        #region Singleton
+        public static ChatListDesignModel Instance => new ChatListDesignModel();
+        #endregion
+
+        #region Constructor
+        public ChatListDesignModel()
+        {
+            Items = new List<ChatListItemViewModel>
+            {
+                new ChatListItemViewModel
+                {
+                    Name = "Luke",
+                    Initials = "LM",
+                    Message = "This chat app is awesome! I bet it will be fast too.",
+                    ProfilePictureRGB = "3099c5"
+                },
+                new ChatListItemViewModel
+                {
+                    Name = "Jesse",
+                    Initials = "JA",
+                    Message = "Hey dude, here are the new icons",
+                    ProfilePictureRGB = "fe4503"
+                },
+                new ChatListItemViewModel
+                {
+                    Name = "Parnell",
+                    Initials = "PL",
+                    Message = "The new server is up, got 192.168.1.1",
+                    ProfilePictureRGB = "00d405"
+                },
+            };
+        }
+        #endregion
+    }
+}
