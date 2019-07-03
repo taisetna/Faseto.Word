e11d12ec9b48b8cb571a3ad59b4dd861a9ddb592
ChangHyun Choi <hessorikng@hanmail.net>
2019-07-03 23:35:38 +0900

    ChatMessageList, ChatMessageListItem 추가

---
+++ Faseto.Word/Faseto.Word/Controls/Chat/ChatMessage/ChatMessageListItemControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// ChatMessageListItemControl.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ChatMessageListItemControl : UserControl
+    {
+        public ChatMessageListItemControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
