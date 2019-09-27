fe5ff7e810d9df63d770301d921d7b58eb8d2936
Chang Hyun Choi <taisetna@nate.com>
2019-09-27 22:50:34 +0900

    향상된 크로스 컨트롤 동기화 기능 구현

--- Faseto.Word/Faseto.Word/Controls/Input/TextEntryControl.xaml.cs
+++ Faseto.Word/Faseto.Word/Controls/Input/TextEntryControl.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Fasetto.Word
 {
@@ -7,9 +10,51 @@ namespace Fasetto.Word
     /// </summary>
     public partial class TextEntryControl : UserControl
     {
+        #region Dependency Properties
+
+        public GridLength LabelWidth
+        {
+            get => (GridLength)GetValue(LabelWidthProperty);
+            set => SetValue(LabelWidthProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for LabelWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelWidthProperty =
+            DependencyProperty.Register("LabelWidth", typeof(GridLength), typeof(TextEntryControl), new PropertyMetadata(GridLength.Auto, LabelWidthChangedCallback));
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public TextEntryControl()
         {
             InitializeComponent();
         }
+        #endregion
+
+        #region Dependency Callbacks
+        /// <summary>
+        /// Called when the label width has changed
+        /// </summary>
+        /// <param name="d"></param>
+        /// <param name="e"></param>
+        public static void LabelWidthChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            try
+            {
+                (d as TextEntryControl).LabelColumnDefinition.Width = (GridLength)e.NewValue;
+            }
+            catch(Exception ex)
+            {
+                Debugger.Break();
+
+                (d as TextEntryControl).LabelColumnDefinition.Width = GridLength.Auto;
+            }
+        }
+
+        #endregion
     }
 }
---
+++ Faseto.Word/Fasetto.Word.Core/ViewModel/Application/Design/SettingsDesignModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Fasetto.Word.Core
+{
+    /// <summary>
+    /// The design-time data for a <see cref="ChatListViewModel"/>
+    /// </summary>
+    public class SettingsDesignModel : SettingsViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static SettingsDesignModel Instance => new SettingsDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public SettingsDesignModel()
+        {
+        }
+
+        #endregion
+    }
+}
